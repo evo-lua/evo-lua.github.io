151e83101d6d4da4073a1ea0f9443f7bced6980c
RDW <duckwhale@mail.ru>
2022-05-28 10:22:51 +0200

    UI: Update the default docusaurus landing page text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Evo.lua',
+  tagline: 'This website is under construction!',
+  url: 'https://evo-lua.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
