c6f33cd6f0a22cc1fe68d956e5b73e0a6bda7222
RDW <duckwhale@mail.ru>
2022-05-28 11:24:01 +0200

    UI: Update the default docusaurus landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Placeholder Title',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This documentation website is under construction. It's unfinished. A work in progress. Not final. Subject to change. Incomplete. The contents are a stand-in, a surrogate, a mere substitute for what is yet to come. The documentation itself is also wholly undeveloped. It is simply not ready. No matter how you put it, there's currently nothing on these pages that would be worth looking at. Nada. Nil. Do you have too much time? Why do you keep reading this silly and senseless paragraph? It purely exists to fill the blank space. Sometimes I really don't understand people on the internet.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Placeholder Title',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        This documentation website is under construction. It's unfinished. A work in progress. Not final. Subject to change. Incomplete. The contents are a stand-in, a surrogate, a mere substitute for what is yet to come. The documentation itself is also wholly undeveloped. It is simply not ready. No matter how you put it, there's currently nothing on these pages that would be worth looking at. Nada. Nil. Do you have too much time? Why do you keep reading this silly and senseless paragraph? Did you honestly think I would make up more than one of these? Well, I guess persistence must be rewarded.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Placeholder Title',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        This documentation website is under construction. It's unfinished. A work in progress. Not final. Subject to change. Incomplete. The contents are a stand-in, a surrogate, a mere substitute for what is yet to come. The documentation itself is also wholly undeveloped. It is simply not ready. No matter how you put it, there's currently nothing on these pages that would be worth looking at. Nada. Nil. Do you have too much time? Oh, by the way: Did you hear that a long time ago, in 1998, The Undertaker threw Mankind off Hell In A Cell, and plummeted 16 ft through an announcer's table?
       </>
     ),
   },
@@ -39,9 +36,9 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
-      <div className="text--center padding-horiz--md">
+      <div>
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/getting-started">
+            This Button Doesn't Yet Work
           </Link>
         </div>
       </div>
