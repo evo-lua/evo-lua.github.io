4d4a999bf6f7c153631ef4e4d47556540644b360
RDW <duckwhale@mail.ru>
2022-07-11 16:18:32 +0200

    Update API.jsx

--- src/components/ApiDocumentation/API.jsx
+++ src/components/ApiDocumentation/API.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import styles from './styles.module.css';
 
-export const Function = ({name, children}) => (
-	<p>
+export const Function = ({children}) => (
+	<div className={styles.apiDocumentationSection}>
 		{children}
-	</p>
+	</div>
   );
 
 class Parameter extends React.Component {
@@ -13,12 +13,21 @@ class Parameter extends React.Component {
 		super(props);
 	}
 	render() {
+
+		let displayName = this.props.name;
+		let displayType = this.props.type;
+
+		if(this.props.varargs) {
+			displayName = "...";
+			displayType = <a href="https://www.lua.org/pil/5.2.html" target="_blank" alt="Programming in Lua: Variable Arguments">varargs</a>;
+		}
+
 		return (
 			<tr>
 				<td className={styles.tdCounter}>{this.props.index}</td>
-				<td>{this.props.name}</td>
-				<td>{this.props.type ? this.props.type : "any"}{this.props.optional ? "?" : ""}</td>
-				<td>{this.props.fallback ? this.props.fallback : "---"}</td>
+				<td>{displayName}</td>
+				<td>{displayType ? displayType : "any"}{this.props.optional ? "?" : ""}</td>
+				<td>{this.props.fallback ? this.props.fallback : ""}</td>
 			</tr>
 		)
 	}
@@ -32,28 +41,27 @@ class Parameters extends React.Component {
 		}
 	}
 	render(){
-
 		return(
-			<table className={styles.tdCounterReset}>
-				Arguments:
-			<thead>
-				<tr>
-				</tr>
-				<tr>
-					<th>#</th>
-					<th>Name</th>
-					<th>Type</th>
-					<th>Fallback</th>
-				</tr>
-			</thead>
-			<tbody>
-				{
-					this.props.children
-				}
-			</tbody>
-		</table>
-			)
-		}
+			<table className={styles.functionParametersTable}>
+				<tbody className={[styles.tdCounterReset, styles.functionParametersTableBody].join(" ")}>
+					<tr className={styles.functionParametersFirstRow}>
+						<th colspan="4">
+							Arguments
+						</th>
+					</tr>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Type</th>
+						<th>Fallback</th>
+					</tr>
+					{
+						this.props.children
+					}
+				</tbody>
+			</table>
+		)
+	}
 }
 
   export const Parameters2 = ({children}) => (
