cf5e50c2e0fc59cc0aa61b741cb1a1d821de724b
RDW <duckwhale@mail.ru>
2022-07-11 15:16:57 +0200

    css counter, react too convoluted

---
+++ src/components/ApiDocumentation/API.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+import styles from './styles.module.css';
+
+export const Function = ({name, children}) => (
+	<p>
+		{children}
+	</p>
+  );
+
+  export const Parameter2 = ({index, name, type, optional, fallback}) => (
+	  <tr>
+		<td>{1}</td>
+		<td>{name}</td>
+		<td>{type ? type : "any"}{optional ? "?" : ""}</td>
+		<td>{fallback}</td>
+	</tr>
+  );
+
+class Parameter extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<tr>
+				<td className={styles.tdCounter}>{this.props.index}</td>
+				<td>{this.props.name}</td>
+				<td>{this.props.type ? this.props.type : "any"}{this.props.optional ? "?" : ""}</td>
+				<td>{this.props.fallback}</td>
+			</tr>
+		)
+	}
+}
+
+class Parameters extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			lastRenderedChildIndex: 0
+		}
+	}
+	render(){
+
+		return(
+			<table className={styles.tdCounterReset}>
+				Arguments:
+			<thead>
+				<tr>
+				</tr>
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Type</th>
+					<th>Fallback</th>
+				</tr>
+			</thead>
+			<tbody>
+				{
+					this.props.children
+				}
+			</tbody>
+		</table>
+			)
+		}
+}
+
+  export const Parameters2 = ({children}) => (
+	<table>
+		<thead>
+			<tr>
+				<th colspan="4">
+					Arguments
+				</th>
+			</tr>
+			<tr>
+				<th>#</th>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Fallback</th>
+			</tr>
+		</thead>
+		<tbody>
+			{
+				children
+			}
+		</tbody>
+	</table>
+  );
+
+export { Parameters, Parameter};
