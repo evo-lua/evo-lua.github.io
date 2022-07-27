0652fcac8c821acf68aea35f37da2fa516e9da1b
RDW <duckwhale@mail.ru>
2022-07-27 07:26:36 +0200

    UI: Update the temporary landing page link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started">
-            This Button Doesn't Yet Work
+            to="/docs">
+            Let's read some documentation! (WIP)
           </Link>
         </div>
       </div>
