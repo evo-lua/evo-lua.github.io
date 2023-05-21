b056d49a25e3d75a263e12fd7c9188a156f01801
RDW <duckwhale@mail.ru>
2023-05-21 13:26:30 +0200

    Cleanup: Remove the now-unused time difference computations

--- src/components/LatestRelease/index.jsx
+++ src/components/LatestRelease/index.jsx
@@ -8,38 +8,6 @@ function LatestReleaseButton() {
   const [releaseInfo, setReleaseInfo] = useState("");
   const [assets, setAssets] = useState([]);
 
-  const getAccurateTimeDifference = (timeDifference) => {
-    const seconds = Math.floor(timeDifference / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-
-    if (hours > 0) {
-      return `${hours} hours ago`;
-    } else if (minutes > 0) {
-      return `${minutes} minutes ago`;
-    } else {
-      return `${seconds} seconds ago`;
-    }
-  };
-
-  const getTimeDifferenceInDays = (timeDifference) => {
-    return `${Math.floor(timeDifference / MILLISECONDS_PER_DAY)} days ago`;
-  };
-
-  const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
-
-  const getHumanReadableTimeDifference = (releaseDate) => {
-    const now = new Date();
-    const timeDifference = now - releaseDate;
-
-    const wasLastReleaseToday = timeDifference < MILLISECONDS_PER_DAY;
-    const humanReadableTimeDiff = wasLastReleaseToday
-      ? getAccurateTimeDifference(timeDifference)
-      : getTimeDifferenceInDays(timeDifference);
-
-    return humanReadableTimeDiff;
-  };
-
   useEffect(() => {
     fetch("https://api.github.com/repos/evo-lua/evo-runtime/releases/latest")
       .then((response) => response.json())
